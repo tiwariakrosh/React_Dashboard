2940636e24f34fafa86e61aa6f26ed232d776361
Akrosh Tiwari <metiwariakrosh02@gmail.com>
2023-04-05 00:58:03 +0545

    Added theme toogle on topbar and work on theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import { ColorModeContext, useMode } from "./theme";
+// import { CssBaseLine, ThemeProvider } from '@mui/material'
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="App">
-      <h1>Hello Nepal </h1>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <main className="context">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Box, IconButton, useTheme } from '@mui/material';
+import { ColorModeContext, tokens } from '../../theme';
+import InputBase from '@mui/material/InputBase';
+import Search from '@mui/icons-material/Search';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+
 
 const Topbar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+
     return (
-        <div>Topbar</div>
+        <Box display="flex" justifyContent="space-between" p={2}>
+            <Box Box display='flex' backgroundColor={colors.primary[400]} borderRadius='4px' >
+                <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+                <IconButton type='button' sx={{ p: 1 }}>
+                    <Search />
+                </IconButton>
+            </Box >
+            {/* Icons */}
+            <Box Box display='flex' >
+                <IconButton>
+                    <LightModeOutlinedIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsOutlinedIcon />
+                </IconButton>
+                <IconButton>
+                    <SettingsOutlinedIcon />
+                </IconButton>
+                <IconButton>
+                    <PersonOutlinedIcon />
+                </IconButton>
+            </Box >
+        </Box>
     )
 }
 
