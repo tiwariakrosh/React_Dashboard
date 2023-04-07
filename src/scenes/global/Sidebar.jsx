cea6616cd1f4b4c07e200c556fed22bbbf4dbddb
Akrosh Tiwari <metiwariakrosh02@gmail.com>
2023-04-07 20:38:39 +0545

    small changes

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,23 +1,91 @@
-import React from 'react'
-import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
-import 'react-pro-sidebar/dist/css/styles.css';
+import React, { useState } from 'react'
+import { ProSidebarProvider, Menu, MenuItem } from 'react-pro-sidebar';
+
+// import 'react-pro-sidebar/dist/styles/StyledUl';
 import { Box, IconButton, Typography, useTheme } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { tokens } from '../../theme';
+// import profile from '';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
 import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
 import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
-import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
-import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
-import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
-import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
-import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
-import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
+import PieChartOutlinedIcon from '@mui/icons-material/PieChartOutlined';
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 
 const Sidebar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [selected, setSelected] = useState("Dashboard");
     return (
-        <div>Sidebar</div>
+        <Box sx={{
+            "& .pro-slider-inner": {
+                backgroundColor: `${colors.primary[400]} !important`
+            },
+            "& .pro-icon-wrapper": {
+                backgroundColor: "transparent !important"
+            },
+            "& .pro-inner-item": {
+                padding: "5px 35px 5px 20px !important"
+            },
+            "& .pro-inner-item:hover": {
+                color: "#868dfb !important"
+            },
+            "& .pro-inner-item.active": {
+                color: "#6870fa !important"
+            },
+        }}>
+            <ProSidebarProvider collapsed={isCollapsed}>
+                <Menu iconShape="square">
+                    {/* Logo and menu icon */}
+                    <MenuItem
+                        onClick={() => setIsCollapsed(!isCollapsed)}
+                        icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+                        style={{
+                            margin: '10px 0 20px 0',
+                            color: colors.grey[100],
+                        }}
+                    >
+                        {!isCollapsed && (
+                            <Box
+                                display="flex"
+                                justifyContent="space-between"
+                                alignItems="center"
+                                ml="15px"
+                            >
+                                <Typography variant="h3" color={colors.grey[100]}>
+                                    ADMINS
+                                </Typography>
+                                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                                    <MenuOutlinedIcon />
+                                </IconButton>
+                            </Box>
+                        )}
+                    </MenuItem>
+                    {/* user */}
+                    {!isCollapsed && (
+                        <Box mb="25px">
+                            <Box display="flex" justifyContent="space-between"
+                                alignItems="center">
+                                <img src='/assets/girl-profile.jpg'
+                                    alt="user-profile"
+                                    width="100px"
+                                    height="100px"
+                                    style={{ cursor: 'pointer', borderRadius: "50px", objectFit: "cover" }}
+                                />
+                            </Box>
+                        </Box>
+                    )}
+                </Menu>
+            </ProSidebarProvider>
+        </Box>
     )
 }
 
